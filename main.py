9703d9116cc0f63f1ca3deb14079b035aeca78fa
enzobertel1 <enzobertel@gmail.com>
2024-06-04 17:36:19 +0200

    V1

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ API_KEY = "RGAPI-c8339727-14f9-4ab4-815a-ea966abf14a6"
 SHEET_ID = "1MtUAVv3p2X0syIAfP4Kt62L16Gdder6X66aAjpzKR8M"
 
 
+
 creds = Credentials.from_service_account_file("credentials.json",scopes=scopes)
 client = gspread.authorize(creds)
 
@@ -28,189 +29,65 @@ def createNewPlayer(pseudo,tag,role,roster):
     p.createWS(SHEET)
 
 
-    
-    
-    
-        
-# Créé et formate un joueur dans l'excel de la façon par défaut
-def joueur(ws,player:Player):
-    ws.update_acell("A1","Pseudo:")
-    ws.update_acell("B1",player.name)
-    ws.update_acell("C1","Tag:")
-    ws.update_acell("D1",player.tag)
-    ws.update_acell("E1","PUUID:")
-    ws.update_acell("F1",player.puuid)
-    ws.update_acell("A2","Role:")
-    ws.update_acell("B2",player.role.value)
-    ws.update_acell("C2","Equipe:")
-    ws.update_acell("D2",player.roster.value)
-    
 
-    ws.update_acell("A3","RANG")
 
-    ws.update_acell("A5","Solo Queue Classée")
-    ws.update_acell("B5","Moyenne Globale")
-    ws.update_acell("C5","Moyenne dernieres games")
-    ws.update_acell("D5","Game comptées")
-    ws.update_acell("D6",0)
-
-    ws.update_acell("A6","CS/minutes")
-    ws.update_acell("A7","Kill participation")
-    ws.update_acell("A8","Damage")
-    ws.update_acell("A9","Damage Objectif")
-    ws.update_acell("A10","Morts / Minutes")
-    ws.update_acell("A11","KDA")
-    ws.update_acell("A12","Vision Score")
-
-    ws.update("B6:C12",[[0,0]for i in range(6,13)])
-    
-
-    ws.format("A3",{
-        "backgroundColor": {
-            "red": 76/255,
-            "green":204/255,
-            "blue": 127/255
-        },
-    })  
-    ws.format("B3:C3",{
-        "backgroundColor": {
-            "red": 183/255,
-            "green":225/255,
-            "blue": 205/255
-        },
-    })   
-
-    ws.format("A5:D5",{
-        "backgroundColor": {
-            "red": 255/255,
-            "green":109/255,
-            "blue": 1/255
-        },
-        "horizontalAlignment": "CENTER",
-        "textFormat": {
-            "foregroundColor": {
-                "red": 1.0,
-                "green": 1.0,
-                "blue": 1.0
-            },
-            "fontSize": 12,
-            "bold": True
-    }
-    })   
-
-    ws.format("A6:D12",{
-        "backgroundColor": {
-            "red": 255/255,
-            "green":229/255,
-            "blue": 153/255
-        },
-        "horizontalAlignment": "CENTER",
-    })   
+     
 
 #Mise à jour des stats de chaque joueur dans l'excel
 def update_players():
     wsts = SHEET.worksheets()
-    today = datetime.datetime.today().timestamp()-(24*60*60)
-    todayStr= str(int(today))
-    print(todayStr)
+    
+    
     for ws in wsts:
+        
         if ws.acell("E1").value == "PUUID:":
-            print("Mise à jour des données de "+ws.acell("B1").value)
+            print("\nMise à jour des données de "+ws.acell("B1").value)
+            match ws.acell("B2").value:
+                case "Support":
+                   match  ws.acell("D2").value:
+                        case "Valhalla":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.SUPPORT,Roaster.VALHALLA,ws.acell("F1").value,ws)
+                            p.update()
+                        case "Helheim":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.SUPPORT,Roaster.HELHEIM,ws.acell("F1").value,ws)
+                            p.update()
+                case "Mid":
+                    match  ws.acell("D2").value:
+                        case "Valhalla":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.MID,Roaster.VALHALLA,ws.acell("F1").value,ws)
+                            p.update()
+                        case "Helheim":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.MID,Roaster.HELHEIM,ws.acell("F1").value,ws)
+                            p.update()
+                case "Top":
+                    match  ws.acell("D2").value:
+                        case "Valhalla":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.TOP,Roaster.VALHALLA,ws.acell("F1").value,ws)
+                            p.update()
+                        case "Helheim":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.TOP,Roaster.HELHEIM,ws.acell("F1").value,ws)
+                            p.update()
+                case "Jungle":
+                    match  ws.acell("D2").value:
+                        case "Valhalla":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.JUNGLE,Roaster.VALHALLA,ws.acell("F1").value,ws)
+                            p.update()
+                        case "Helheim":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.JUNGLE,Roaster.HELHEIM,ws.acell("F1").value,ws)
+                            p.update()
+                case "Adc":
+                    match  ws.acell("D2").value:
+                        case "Valhalla":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.ADC,Roaster.VALHALLA,ws.acell("F1").value,ws)
+                            p.update()
+                        case "Helheim":
+                            p = Player(ws.acell("B1").value,ws.acell("D1").value,Role.ADC,Roaster.HELHEIM,ws.acell("F1").value,ws)
+                            p.update()
+    print("\nJoueurs mis à jour.")
 
-            ws.update([[0] for i in range(6,13)],"C6:C12")
+                
+                            
 
-            puuid = ws.acell("F1").value
-            url = URL+"lol/match/v5/matches/by-puuid/"+puuid+"/ids"
-
-            nb_games=0
-
-            req = url+"?start=0&count=20&api_key="+API_KEY
-
-            response = requests.get(req)
-            if response.ok:
-                result = response.json()
-                for match_id in result:
-                    
-
-                    url = URL+"lol/match/v5/matches/"+match_id
-                    response = requests.get(url+"?start=0&count=20&api_key="+API_KEY)
-                    if response.ok:
-                        res = response.json()
-                        if res["info"]["gameMode"] == "CLASSIC" and res["info"]["gameType"] == "MATCHED_GAME":
-                            nb_games+=1
-                            print('Match ',match_id)
-                            #Incrémente le nombre de games jouées
-                            game_played = float(ws.acell("D6").value.replace(",","."))+1
-                            ws.update_acell("D6",game_played)
-
-                            indexPlayer = res["metadata"]["participants"].index(puuid)
-                            participants = res["info"]["participants"][indexPlayer]
-
-                            #Minions/Minute
-                            old_mpm = float(ws.acell("B6").value.replace(",","."))
-                            mpm = participants["totalMinionsKilled"] / (res["info"]["gameDuration"]/60)
-                            mean_mpm = ((old_mpm*(game_played-1)) + mpm) /game_played
-                            ws.update_acell("B6", mean_mpm)
-                            ws.update_acell("C6", float(ws.acell("C6").value.replace(",",".")) + mpm)
-
-                            #Kill Participation
-                            old_kp = float(ws.acell("B7").value.replace(",","."))
-                            kp = (participants["assists"] +participants["kills"]) / sum([k["kills"]  for k in res["info"]["participants"] if k["teamId"] == participants["teamId"]] )
-                            mean_kp = ((old_kp*(game_played-1)) + kp) /game_played
-                            ws.update_acell("B7", mean_kp)
-
-                            ws.update_acell("C7", float(ws.acell("C7").value.replace(",",".")) + kp)
-
-                            #Damage
-                            old_dmg = float(ws.acell("B8").value.replace(",","."))
-                            dmg = participants["totalDamageDealtToChampions"] / (res["info"]["gameDuration"]/60)
-                            mean_dmg = ((old_dmg*(game_played-1)) + dmg) /game_played
-                            ws.update_acell("B8", mean_dmg)
-
-                            ws.update_acell("C8", float(ws.acell("C8").value.replace(",",".")) + dmg)
-
-                            #DamageObjective
-                            old_dmgObj = float(ws.acell("B9").value.replace(",","."))
-                            dmgObj = participants["damageDealtToObjectives"]
-                            mean_dmgObj = ((old_dmgObj*(game_played-1)) + dmgObj) /game_played
-                            ws.update_acell("B9", mean_dmgObj)
-
-                            ws.update_acell("C9", float(ws.acell("C9").value.replace(",",".")) + dmgObj)
-
-                            #Deaths/minute
-                            old_dpm = float(ws.acell("B10").value.replace(",","."))
-                            dpm = participants["deaths"] / (res["info"]["gameDuration"]/60)
-                            mean_dpm = ((old_dpm*(game_played-1)) + dpm) /game_played
-                            ws.update_acell("B10", mean_dpm)
-
-                            ws.update_acell("C10", float(ws.acell("C10").value.replace(",",".")) + dpm)
-
-                            #KDA
-                            old_kda = float(ws.acell("B11").value.replace(",","."))
-                            kda = (participants["assists"] +participants["kills"])/ participants["deaths"]
-                            mean_kda = ((old_kda*(game_played-1)) + kda) /game_played
-                            ws.update_acell("B11", mean_kda)
-
-                            ws.update_acell("C11", float(ws.acell("C11").value.replace(",",".")) + kda)
-
-                            #Vision score
-                            old_vs = float(ws.acell("B12").value.replace(",","."))
-                            vs = participants["visionScore"]
-                            mean_vs = ((old_vs*(game_played-1)) + vs) /game_played
-                            ws.update_acell("B12", mean_vs)
-
-                            ws.update_acell("C12", float(ws.acell("C12").value.replace(",",".")) + vs)
-
-                            print("CS/Min=",mpm,";Kill Participation=",kp,";Damage=",dmg,";Damage Obj=",dmgObj,";Morts/Min=",dpm,";KDA=",kda,";Vision=",vs)
-
-                            time.sleep(60)
-   
-
-                if nb_games < 20 and nb_games >0:       
-                    for k in range (6,13):
-                        old = float(ws.acell("C"+str(k)).value.replace(",","."))
-                        print(old)
-                        ws.update_acell("C"+str(k), old/nb_games)
                     
 
             
@@ -231,14 +108,49 @@ if __name__ == "__main__":
             case "1":
                 pseudo = input("entrer le nom du joueur : ")
                 tag = input("entrer le tag du joueur : ")
-                strRole= input("entrer le rôle du joueur : ")
+                strRole= input("entrer le rôle du joueur (S/T/J/A/M): ")
+                strRoster= input("entrer le roster du joueur (V/H) : ")
 
-                print()
+                if tag != "" and pseudo != "":
+                    match strRole.upper():
+                        case "S"|"SUPPORT"|"SUPP":
+                            match strRoster.upper():
+                                case "V"|"VALHALLA":
+                                    createNewPlayer(pseudo,tag,Role.SUPPORT,Roaster.VALHALLA)
+                                case "H"|"HELHEIM":
+                                    createNewPlayer(pseudo,tag,Role.SUPPORT,Roaster.HELHEIM)
+                        case "T"|"TOP":
+                            match strRoster.upper():
+                                case "V"|"VALHALLA":
+                                    createNewPlayer(pseudo,tag,Role.TOP,Roaster.VALHALLA)
+                                case "H"|"HELHEIM":
+                                    createNewPlayer(pseudo,tag,Role.TOP,Roaster.HELHEIM)
+                        case "J"|"JUNGLE"|"JUNGLER":
+                            match strRoster.upper():
+                                case "V"|"VALHALLA":
+                                    createNewPlayer(pseudo,tag,Role.JUNGLE,Roaster.VALHALLA)
+                                case "H"|"HELHEIM":
+                                    createNewPlayer(pseudo,tag,Role.JUNGLE,Roaster.HELHEIM)
 
-                if strRole != "" and tag != "" and pseudo != "":
-                    createNewPlayer(pseudo,tag)
+                        case "M"|"MID":
+                            match strRoster.upper():
+                                case "V"|"VALHALLA":
+                                    createNewPlayer(pseudo,tag,Role.MID,Roaster.VALHALLA)
+                                case "H"|"HELHEIM":
+                                    createNewPlayer(pseudo,tag,Role.MID,Roaster.HELHEIM)
+
+                        case "A"|"ADC"|"ADCARRY":
+                            match strRoster.upper():
+                                case "V"|"VALHALLA":
+                                    createNewPlayer(pseudo,tag,Role.ADC,Roaster.VALHALLA)
+                                case "H"|"HELHEIM":
+                                    createNewPlayer(pseudo,tag,Role.ADC,Roaster.HELHEIM)
+                        
+                        case _ :
+                            print("Rôle inconnu\n")
             case "2":
                 update_players()
+            
 
         rep = input("Continuer ? (Y/n) : ")
         if rep == "n" or rep == "N":
--- player.py
+++ player.py
@@ -1,11 +1,22 @@
 import requests
 from enum import Enum
+import time
 
 URL = "https://europe.api.riotgames.com/"
 API_KEY = "RGAPI-c8339727-14f9-4ab4-815a-ea966abf14a6"
 
 NB_ROWS = 100
 NB_COLS = 20
+
+CELL_VALUES = {
+                "minionsPerMinutes": "6",
+                "killParticipation": "7",
+                "damagePerMinute":"8",
+                "damageObjectives":"9",
+                "deathsPerMinute":"10",
+                "kda":"11",
+                "vision":"12"
+            }
 
 class Role(Enum):
     SUPPORT = "Support"
@@ -18,8 +29,103 @@ class Roaster(Enum):
     VALHALLA = "Valhalla"
     HELHEIM = "Helheim"
 
+def getMatchInfo(match_id,puuid):
+    url = URL+"lol/match/v5/matches/"+match_id
+    response = requests.get(url+"?api_key="+API_KEY)
+    if response.ok:
+        res = response.json()
+        
+
+        if res["info"]["gameMode"] == "CLASSIC" and res["info"]["gameType"] == "MATCHED_GAME":
+
+            indexPlayer = res["metadata"]["participants"].index(puuid)
+            durationInMinutes = res["info"]["gameDuration"]/60
+
+            player = res["info"]["participants"][indexPlayer]
+            return {
+                "minionsPerMinutes": player["totalMinionsKilled"] / (durationInMinutes),
+                "killParticipation": (player["assists"] +player["kills"]) / sum([k["kills"]  for k in res["info"]["participants"] if k["teamId"] == player["teamId"]]),
+                "damagePerMinute":player["totalDamageDealtToChampions"]/durationInMinutes,
+                "damageObjectives":player["damageDealtToObjectives"],
+                "deathsPerMinute":player["deaths"]/durationInMinutes,
+                "kda":(player["assists"] +player["kills"])/ player["deaths"] if player["deaths"]!=0 else 1,
+                "vision":player["visionScore"]
+            }
+        else:
+            raise BaseException("La partie n\'est pas en mode normal")
+        
+def replaceMeanInWs(ws,place:str,value:float,nbGames:int):
+    old = float(ws.acell(place).value.replace(",","."))
+    new = ((old*(nbGames-1)) + value) /nbGames
+    ws.update_acell(place,new)
+
+def fill_ws(p):
+
+    ws=p.ws
+    ws.update_acell("A1","Pseudo:")
+    ws.update_acell("B1",p.name)
+    ws.update_acell("C1","Tag:")
+    ws.update_acell("D1",p.tag)
+    ws.update_acell("E1","PUUID:")
+    ws.update_acell("F1",p.puuid)
+    ws.update_acell("A2","Role:")
+    ws.update_acell("B2",p.role.value)
+    ws.update_acell("C2","Equipe:")
+    ws.update_acell("D2",p.roster.value)
+    
+
+    ws.update_acell("A3","RANG")
+
+    ws.update_acell("A5","Solo Queue Classée")
+    ws.update_acell("B5","Moyenne Globale")
+    ws.update_acell("C5","Moyenne dernieres games")
+    ws.update_acell("D5","Game comptées")
+    ws.update_acell("D6",0)
+
+    for k,v in CELL_VALUES.items():
+        ws.update_acell("A"+v,k)
+
+    ws.update("B6:C12",[[0,0]for i in range(6,13)])
+    
+
+    ws.format("A3",{
+        "backgroundColor": {
+            "red": 76/255,
+            "green":204/255,
+            "blue": 127/255
+        },
+    })  
+   
+
+    ws.format("A5:D5",{
+        "backgroundColor": {
+            "red": 255/255,
+            "green":109/255,
+            "blue": 1/255
+        },
+        "horizontalAlignment": "CENTER",
+        "textFormat": {
+            "foregroundColor": {
+                "red": 1.0,
+                "green": 1.0,
+                "blue": 1.0
+            },
+            "fontSize": 12,
+            "bold": True
+    }
+    })   
+
+    ws.format("A6:D12",{
+        "backgroundColor": {
+            "red": 255/255,
+            "green":229/255,
+            "blue": 153/255
+        },
+        "horizontalAlignment": "CENTER",
+    })  
+
 class Player :
-    def __init__(self,name:str,tag:str,role:Role,roster:Roaster) -> None:
+    def __init__(self,name:str,tag:str,role:Role,roster:Roaster,puuid=None,ws=None,) -> None:
         # init values
         self.name = name
         self.tag = tag
@@ -27,11 +133,11 @@ class Player :
         self.role = role
 
         # lateinit values
-        self.ws = None
-        self.puuid = None
+        self.ws = ws
+        self.puuid = puuid
     
     def createWS(self,sheet):
-
+        self.getPUUID()
         ws = None
         try:
             ws = sheet.worksheet(self.roster.value+"_"+self.role.value)
@@ -50,7 +156,13 @@ class Player :
             
         self.ws = ws
 
+        fill_ws(self)
+
+        print(self.ws.col_values(7))
+
         return ws
+    
+    
 
         
 
@@ -68,12 +180,11 @@ class Player :
         if self.ws != None :
             self.puuid = self.ws.acell("F1").value
             return self.puuid
+        
         response = requests.get(URL+"riot/account/v1/accounts/by-riot-id/"+self.name+"/"+self.tag+"?api_key="+API_KEY)
         if response.ok:
             self.puuid = response.json()["puuid"]
-
             
-
         return self.puuid
     
     def getLastMatchsIds(self):
@@ -84,7 +195,12 @@ class Player :
             raise BaseException("Exception : getMatchIds => Le joueur n'a pas de PUUID")
         
         #Regardez le tableau d'ids dans la colonne G
-        matchsAlreadyDone = self.ws.col_values(7)
+        
+        matchsAlreadyDone = []
+
+        for i in range(1,21):
+            if (self.ws.acell("G"+str(i)).value == None):
+                matchsAlreadyDone.append(self.ws.acell("G"+str(i)).value)
 
         url = URL+"lol/match/v5/matches/by-puuid/"+self.puuid+"/ids"
         
@@ -94,7 +210,7 @@ class Player :
 
         if response.ok:
                 result = response.json()
-                return filter((lambda x : (x not in matchsAlreadyDone)), result)
+                return list(filter((lambda x : (x not in matchsAlreadyDone)), result))
         else:
             raise BaseException("Exception : getMatchIds => La requête n'a pas fonctionnée")
         
@@ -102,10 +218,45 @@ class Player :
         try:
             matchIds = self.getLastMatchsIds()
             
+            if len(matchIds)> 0:
+                self.ws.update([[0] for _ in range(6,13)],"C6:C12")
 
             nb_games=0
             for match_id in matchIds :
-                pass
+                try:
+                    
+                    match = getMatchInfo(match_id,self.puuid)
+
+                    #Incrémente le nombre de games jouées
+                    game_played = float(self.ws.acell("D6").value.replace(",","."))+1
+                    self.ws.update_acell("D6",game_played)
+                    nb_games+=1
+
+                    fill = False
+                    for i in range(1,21):
+                        if (self.ws.acell("G"+str(i)).value == None):
+                            self.ws.update_acell("G"+str(i),match_id)
+                            fill = True
+                            break
+
+                    if not fill:
+                        for i in range(1,21):
+                            self.ws.update_acell("G"+str(i),self.ws.acell("G"+str(i+1)).value)
+
+                        self.ws.update_acell("G20",match_id)
+
+                    print("Partie : ",match_id)
+                                
+
+                    for k,v in CELL_VALUES.items():
+                        replaceMeanInWs(self.ws,"B"+v,match[k],game_played)
+                        replaceMeanInWs(self.ws,"C"+v,match[k],nb_games)
+
+
+                    time.sleep(60)
+
+                except BaseException as e:
+                    print(str(e))
         except BaseException as e:
             print(str(e))
 
